f33e79c9ff207ce71dca03ea95f3b5f00d123253
Maccabee Levine <msl321@lehigh.edu>
2022-11-29 09:07:45 -0500

    Set or unset library_staff cookie.

---
+++ staff.php
@@ -0,0 +1,29 @@
+<?php
+$cookie_name = "library_staff";
+
+if (isset($_GET["set"]) && !isset($_COOKIE[$cookie_name])) {
+  setcookie($cookie_name, 1, time() + (60 * 60 * 24 * 365 * 20), "/", "lehigh.edu");
+  header("Refresh:0");
+}
+elseif (isset($_GET["unset"]) && isset($_COOKIE[$cookie_name])) {
+  setcookie($cookie_name, "", time() - 3600, "/", "lehigh.edu");
+  header("Refresh:0");
+}	
+
+if(!isset($_COOKIE[$cookie_name])) {
+  echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+  echo "Cookie '" . $cookie_name . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+<ul>
+<li>
+  <a href="?set">Set the Library Staff cookie.</a>
+</li>
+<li>
+  <a href="?unset">Unset the Library Staff cookie.</a>
+</li>
+</ul>
+
