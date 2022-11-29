46055ff8547724d43064ba42be51a83783b74e0b
Maccabee Levine <msl321@lehigh.edu>
2022-11-29 09:10:38 -0500

    Refactor domain name

--- staff.php
+++ staff.php
@@ -1,18 +1,20 @@
 <?php
 $cookie_name = "library_staff";
+$domain = "lehigh.edu";
 
 if (isset($_GET["set"]) && !isset($_COOKIE[$cookie_name])) {
-  setcookie($cookie_name, 1, time() + (60 * 60 * 24 * 365 * 20), "/", "lehigh.edu");
+  setcookie($cookie_name, 1, time() + (60 * 60 * 24 * 365 * 20), "/", $domain);
   header("Refresh:0");
 }
 elseif (isset($_GET["unset"]) && isset($_COOKIE[$cookie_name])) {
-  setcookie($cookie_name, "", time() - 3600, "/", "lehigh.edu");
+  setcookie($cookie_name, "", time() - 3600, "/", $domain);
   header("Refresh:0");
 }	
 
 if(!isset($_COOKIE[$cookie_name])) {
   echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
+} 
+else {
   echo "Cookie '" . $cookie_name . "' is set!<br>";
   echo "Value is: " . $_COOKIE[$cookie_name];
 }
