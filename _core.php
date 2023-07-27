868c8924f4833c0f8b8f1aaec6a8999c3ff2ceed
Maccabee Levine <msl321@lehigh.edu>
2023-07-27 08:53:36 -0400

    Renew cookie if older than a minute.

--- _core.php
+++ _core.php
@@ -1,15 +1,23 @@
 <?php
 
 $cookie_name = "library_staff";
+$expiry_cookie_name = "library_staff_exp";
 $cookie_duration = (60 * 60 * 24 * 400); # 400 days is spec max
 $domain = "lehigh.edu";
 
-if (isset($_GET["set"]) && !isset($_COOKIE[$cookie_name])) {
-  setcookie($cookie_name, $cookie_value, time() + $cookie_duration, "/", $domain);
-  header("Refresh:0");
+if (isset($_GET["set"])) {
+  $old_exp = intval($_COOKIE[$expiry_cookie_name]) ?? 0;
+  $new_exp = time() + $cookie_duration;
+  if ($old_exp < $new_exp - 60) {
+    setcookie($cookie_name, $cookie_value, $new_exp, "/", $domain);
+    setcookie($expiry_cookie_name, $new_exp, $new_exp, "/", $domain);
+    header("Refresh:0");
+  }
 }
-elseif (isset($_GET["unset"]) && isset($_COOKIE[$cookie_name])) {
+elseif (isset($_GET["unset"]) && 
+  (isset($_COOKIE[$cookie_name]) || isset($_COOKIE[$expiry_cookie_name]))) {
   setcookie($cookie_name, "", time() - 3600, "/", $domain);
+  setcookie($expiry_cookie_name, "", time() - 3600, "/", $domain);
   header("Refresh:0");
 }
 
@@ -20,6 +28,7 @@ else {
   echo "Cookie '" . $cookie_name . "' is set!<br>";
   echo "Value is: " . $_COOKIE[$cookie_name];
 }
+
 ?>
 
 <ul>
